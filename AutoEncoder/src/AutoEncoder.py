7df1d1639c267d3aecf7756ef4ddff9af57be478
aalhag24 <aalhag24@gmail.com>
2019-02-28 19:48:54 -0800

    Completed Training the AutoEncoder

--- AutoEncoder/src/AutoEncoder.py
+++ AutoEncoder/src/AutoEncoder.py
@@ -25,6 +25,7 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
+import tensorflow as tf
 
 # List of file paths
 BinPath = os.path.abspath(os.path.join(os.path.dirname( '__file__' ), '..', 'bin'))
@@ -68,13 +69,58 @@ test_set = convert(test_set)
 training_set = torch.FloatTensor(training_set)
 test_set = torch.FloatTensor(test_set)
 
-# Converting the ratings into binary ratings 1 (liked) or 0 (Not Liked)
-training_set[training_set == 0] = -1
-training_set[training_set == 1] = 0
-training_set[training_set == 2] = 0
-training_set[training_set >= 3] = 1
-    
-test_set[test_set == 0] = -1
-test_set[test_set == 1] = 0
-test_set[test_set == 2] = 0
-test_set[test_set >= 3] = 1
+# Creating the architecture of the Auto Encoder
+class SAE(nn.Module):
+    def __init__(self, ):
+        super(SAE, self).__init__()
+        self.fc1 = nn.Linear(nb_movies, 20)
+        self.fc2 = nn.Linear(20, 10)
+        self.fc3 = nn.Linear(10, 20)
+        self.fc4 = nn.Linear(20, nb_movies)
+        self.activation = nn.Sigmoid()
+    def forward(self, x):
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.activation(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+# Defining the Model
+sae = SAE()
+criterion = nn.MSELoss()
+optimizer = optim.RMSprop(sae.parameters(), lr = 0.01, weight_decay = 0.5)
+
+# Training the Model
+nb_epoch = 200
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(nb_users):
+        input = Variable(training_set[id_user]).unsqueeze(0)
+        target = input.clone()
+        if torch.sum(target.data > 0) >0:
+            output = sae(input)
+            target.require_grad = False
+            output[target == 0] = 0
+            loss = criterion(output, target)
+            mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+            loss.backward()
+            train_loss += np.sqrt(loss.item()*mean_corrector)
+            s +=1.
+            optimizer.step()
+    print('epoch: ' + str(epoch) + '- loss: ' + str(train_loss/s))
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
