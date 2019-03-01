a912c5e16d97e28c0f9fad0bf6deef294339c520
aalhag24 <aalhag24@gmail.com>
2019-02-28 16:49:35 -0800

    Implamented RMSE in Boltzmann

--- BoltzmannMachine/src/BoltzmannMachine.py
+++ BoltzmannMachine/src/BoltzmannMachine.py
@@ -139,7 +139,12 @@ for id_user in range(nb_users):
         s += 1.
 print('test loss: '+str(test_loss/s)) 
     
-    
+# Testing the Correlation with RMSE
+u = np.random.choice([0,1], 100000)
+v = np.random.choice([0,1], 100000)
+u[:50000] = v[:50000]
+sum(u==v)/float(len(u)) # -> you get 0.75
+np.mean(np.abs(u-v)) # -> you get 0.25
     
     
         
